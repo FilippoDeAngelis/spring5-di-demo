d1c2fc0d0bbb3949c01f5f15e077124a86d2dfaa
Filippo De Angelis <filippodeangelis0@gmail.com>
2018-11-05 15:39:06 +0100

    Multiple services mapped with qualifiers annotations

--- src/main/java/guru/springframework/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/controllers/PropertyInjectedController.java
@@ -1,7 +1,9 @@
 package guru.springframework.controllers;
 
+import guru.springframework.services.GreetingService;
 import guru.springframework.services.GreetingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 
@@ -12,10 +14,11 @@ import org.springframework.stereotype.Controller;
 public class PropertyInjectedController {
 
     @Autowired
-    public GreetingServiceImpl greetingService;
+    //this works through reflections: spring sees that the property name is one of a bean and qualifies it automatically
+    public GreetingService greetingServiceImpl;
 
     public String sayHello(){
-        return greetingService.sayGreeting();
+        return greetingServiceImpl.sayGreeting();
     }
 
 }
